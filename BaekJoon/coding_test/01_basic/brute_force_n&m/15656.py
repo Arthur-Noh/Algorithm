935c10265ded789eaf70cbf23979bc7e8d870976
DONGJOON NO <88483892+Arthur-Noh@users.noreply.github.com>
2021-10-25 14:22:55 +0900

    Create 15656.py

---
+++ BaekJoon/coding_test/01_basic/brute_force_n&m/15656.py
@@ -0,0 +1,29 @@
+# 코딩 테스트 준비 - 기초
+# 백준 15656 N 과 M(7)
+# 등급 실버 3
+
+# 문제 해결 아이디어
+# 기존 해결 방식과 동일하고, 백트래킹 방식으로 해결한다.
+# 먼저 numbers를 입력받고 오름차순으로 정렬한다.
+# 그리고 dfs 를 선언해서 s 리스트에 numbers 값을 순차적으로 입력 받는다.
+
+import sys
+n, m = map(int, sys.stdin.readline().split())
+numbers = list(map(int, sys.stdin.readline().split()))
+s = []
+
+numbers.sort()
+
+
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+
+    for i in range(n):
+        s.append(numbers[i])
+        dfs()
+        s.pop()
+
+
+dfs()
