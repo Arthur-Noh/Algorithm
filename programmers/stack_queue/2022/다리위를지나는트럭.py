c86adc9f916918b48c89c6fec723f785a14d404b
DONGJOON NO <88483892+Arthur-Noh@users.noreply.github.com>
2022-04-07 22:01:00 +0900

    Update 다리위를지나는트럭.py

--- programmers/stack_queue/2022/다리위를지나는트럭.py
+++ programmers/stack_queue/2022/다리위를지나는트럭.py
@@ -21,21 +21,21 @@ truck_weights = [7, 4, 5, 6]
 
 def solution(bridge_length, weight, truck_weights):
     time = 0
-    bridge_weight = 0
-    waiting = deque(truck_weights)
-    bridge = deque([0 for _ in range(bridge_length)])
+    bridge_weight = 0  # 다리 위 트럭 무게의 총합
+    waiting = deque(truck_weights)  # 대기중인 트럭들(무게)
+    bridge = deque([0 for _ in range(bridge_length)])  # 다리 (길이만큼 q로 표현)
 
-    while len(waiting) or bridge_weight > 0:
-        remove_truck = bridge.popleft()
+    while len(waiting) or bridge_weight > 0:  # 대기중인 트럭이 있거나, 다리 위가 비지 않았을 때 동안 반복
+        remove_truck = bridge.popleft()  # 다리 위의 트럭 하나를 제거한다.
         bridge_weight -= remove_truck
 
-        if len(waiting) and bridge_weight + waiting[0] <= weight:
-            new_truck = waiting.popleft()
-            bridge_weight += new_truck
-            bridge.append(new_truck)
+        if len(waiting) and bridge_weight + waiting[0] <= weight:  # 대기가 있고, 다리위에 트럭을 올릴 수 있을 때
+            new_truck = waiting.popleft()  # 대기 중인 트럭을 하나 꺼내서
+            bridge_weight += new_truck  # 다리 위에 새 트럭 무게를 추가한다.
+            bridge.append(new_truck)  # 다리에 새 트럭을 추가한다.
 
-        else:
-            bridge.append(0)
+        else:  # 트럭을 올릴 수 없다면
+            bridge.append(0)  # 0을 넣어 다리 길이를 유지한다.
 
         time += 1
     return time
